77f9638bc35e6034f31c307ba44a717bc2a16cf2
your name <you@example.com>
2018-06-20 08:29:23 -0700

    added a refresh button

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(() => {
-
+////////////////// MODAL //////////////////
   //Grabbing Elements
 const $openBtn = $('#openModal');
 const $modal = $('#modal');
@@ -9,19 +9,15 @@ const $closeBtn = $('#close');
 const openModal = () => {
   $modal.css('display', 'block');
 }
-
 const closeModal = () => {
   $modal.css('display', 'none');
 }
 
 //Event Listeners
 $openBtn.on('click', openModal);
-
 $closeBtn.on('click', closeModal);
 
-
 ////////////////// Create Board //////////////////
-//create the divs that will make up my board
   //jquery object board that's a div with a class "board"
   const $board = $("<div>").addClass("board")
   //loop to create 6 rows
@@ -42,21 +38,12 @@ $closeBtn.on('click', closeModal);
   $("body").append($board)
 
 //////////////// Event Listeners ///////////////
-  //when i click on any divs in the board, the first empty circle of the column clicked, change bg color
-  //i need to find the first empty circle of that column
-  //change that circle bg color
-  //the color needs to alternate colors with each click
-  //there needs to be a winCheck after each click
-  //winCheck for 4 of same color vertical, horizontal and diagonals
-  //if winner, disable click function and alert winner
-  //reset game button
-
-  //WHY does the below code alternate the colors?????????????????????????????
     //define alternate
     let alternate = true;
     //when you click on a specific div, an event takes place
     $(".col").on("click", function(event) {
     console.log(event.currentTarget);
+
       //if the variable returns true
       if (alternate === true) {
         //div turns slate blue
@@ -72,9 +59,9 @@ $closeBtn.on('click', closeModal);
     })//end on click function
 
 //////////////// Check for Win ///////////////
-
-
-
+//if four divs of the same background color are touching, that color wins.
+//check for this between each click.
+//if true, alert
 
 
 
@@ -82,48 +69,9 @@ $closeBtn.on('click', closeModal);
 })//endpage
 
 
-
-// //create a class for the game like the alien attack game
-// class Game {
-//   //pass in a selector (when theGame initializes, this all runs/get created... )
-//   constructor(selector) {
-//     //set 6 rows
-//     //set 7 columns to be in the rows
-//     //create the "make a grid" function
-//     this.selector = selector;
-//     this.ROWS = 6;
-//     this.COLUMNS = 7;
-//     this.generateGrid()
-//     // this.eventListeners()
-//   }
-// ///////////** add functions **//////////////
-//   generateGrid() {
-//     // create the grid - function (building up a bunch of divs)
-//     //creates div for connect4
-//     const $grid = $(this.selector)
-//     //generate rows with for loops
-//     for (let row=0; row<this.ROWS; row++){
-//     //create jquery object with class and "data-row" attr and pass in the row index 0-5
-//       const $row = $("<div>").addClass("row").attr("data-row", row)
-//         for (let column=0; column<this.COLUMNS; column++){
-//         //create jquery object with class column empty(to use later) and "data-column" attr and pass in the column index 0-6
-//           const $column = $("<div>").addClass("column empty").attr("data-column", column)
-//           //append columns to the rows
-//           $row.append($column)
-//         } // columns for loop end.
-//           //nest the rows inside the grid
-//           $grid.append($row);
-  //   }//rows for loop end
-  // }//generateGrid end
-
   ///////////** event listeners **////////////
 
-  // .data() ->Store arbitrary data associated with the matched elements or return the value at the named data store for the first element in the set of matched elements.
-
-   // eventListeners(){
-   //  const $grid = $(this.selector)
-    // console.log($grid);
-   //  //define findEmptyCircle as a function
+    // //define findEmptyCircle as a function
     // function findEmptyCircle(column) {
     //   //grab all empty circles in the column selected
     //   //hey jQuery, collect ALL the columns that have the same attr data-column equal to the column index passed in.
@@ -145,10 +93,3 @@ $closeBtn.on('click', closeModal);
     //   const $emptyCircle = findEmptyCircle(column)
     //   $emptyCircle.addClass("player1")
     // })//grid on click end
-
-
- // }//class end
- // initializes Connect4 with name connect4. passes in selector "#connect4"
-    // const connect4 = new Game('#connect4')
-    // //calling generateGrid function.
-    // connect4.generateGrid()
